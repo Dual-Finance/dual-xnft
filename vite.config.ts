ed50c93033654239ccaab4aad0782f306ec84955
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-05-03 10:36:32 -0400

    build: remove manual chunk build strategy, breaks build in prod environment

--- vite.config.ts
+++ vite.config.ts
@@ -18,27 +18,27 @@ export default defineConfig(({ command, mode }) => {
         plugins: [NodeGlobalsPolyfillPlugin({ process: true, buffer: true })],
       },
     },
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            const isModule = id.includes("node_modules");
-            if (isModule && id.includes("@solana")) {
-              return "vendor_solana";
-            }
-            if (isModule && id.includes("@project-serum")) {
-              return "vendor_anchor";
-            }
-            if (isModule && id.includes("@metaplex-foundation")) {
-              return "vendor_metaplex";
-            }
-            if (isModule && id.includes("@tanstack")) {
-              return "vendor_tanstack";
-            }
-          },
-        },
-      },
-    },
+    // build: {
+    //   rollupOptions: {
+    //     output: {
+    //       manualChunks: (id) => {
+    //         const isModule = id.includes("node_modules");
+    //         if (isModule && id.includes("@solana")) {
+    //           return "vendor_solana";
+    //         }
+    //         if (isModule && id.includes("@project-serum")) {
+    //           return "vendor_anchor";
+    //         }
+    //         if (isModule && id.includes("@metaplex-foundation")) {
+    //           return "vendor_metaplex";
+    //         }
+    //         if (isModule && id.includes("@tanstack")) {
+    //           return "vendor_tanstack";
+    //         }
+    //       },
+    //     },
+    //   },
+    // },
     server: {
       port: 9933,
     },
